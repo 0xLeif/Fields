dd131d06947021f50a979849cd7fca5372fd3eb5
zmeriksen <zmeriksen@gmail.com>
2019-09-28 16:10:22 -0500

    Added package

--- Sources/Fields/Fields.swift
+++ Sources/Fields/Fields.swift
@@ -1,3 +1,25 @@
-struct Fields {
-    var text = "Hello, World!"
+//
+//  Fields.swift
+//  Fields
+//
+//  Created by Zach Eriksen on 9/28/19.
+//  Copyright © 2019 oneleif. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0, *)
+public struct NumberField: View {
+    var title: String
+    @Binding var value: Int {
+        didSet {
+            self.interalValue = "\(value)"
+        }
+    }
+    @State private var interalValue: String = ""
+    
+    public var body: some View {
+        TextField(title, text: $interalValue)
+            .keyboardType(.numberPad)
+    }
 }
